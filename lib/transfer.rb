9f48acc3de01635a8fd50aebd4d24ba59b31dea3
Daysha Jones <daysha.vu@icloud.com>
2021-03-19 18:38:25 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -30,6 +30,10 @@ class Transfer
   
   def reverse_transfer
     if status == "complete"
+      @sender.deposit( @amount ) 
+      @receiver.deposit( @amount * -1)
+      @status = "reversed"
+    end
   end
-  
+
 end
